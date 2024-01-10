d846bf70de45ad0f3e7f7ae6a6092d2f763f8560
imamankmr <amanjnvr@gmail.com>
2024-01-10 17:03:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
 function App() {
+  const [counter, setCounter]  = useState(15)
+
+  // let Counter=15
+  const addValue=()=>{
+    setCounter(prevCounter => prevCounter + 1)
+  }
+  const removeValue =()=>{
+    setCounter(counter - 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Counter Value :{counter}</h1>
+      <button onClick={addValue}>Add Value </button>
+    <button onClick={removeValue}> Decrease Value</button>
+    </>
   );
 }
 
